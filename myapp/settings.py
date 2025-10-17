56562700ead508d07296a7c631874933ebbef7c0
renjithmr87 <renjithmr87@proton.me>
2025-10-17 05:54:08 +0530

    Deploy test

--- myapp/settings.py
+++ myapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-m8ju6$vgg)!-2qu73k6!@^hk%evmds@1$%c3kl9n--)1prlha2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['16.16.202.114', '192.168.49.2', '*']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','0.0.0.0']
 
 
 # Application definition
